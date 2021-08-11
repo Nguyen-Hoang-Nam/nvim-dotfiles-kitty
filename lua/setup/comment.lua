97b1973b3ebcde2d6587d0f4eff323f801c6ae36
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-08-11 19:34:40 +0700

    ✨ Support mix comment file type

--- lua/setup/comment.lua
+++ lua/setup/comment.lua
@@ -1,4 +1,15 @@
+local function check_mix_lang()
+    local file_type = vim.api.nvim_buf_get_option(0, 'filetype')
+
+    return file_type == 'tsx' or file_type == 'svelte' or file_type == 'vue' or file_type == 'html'
+end
+
 require('nvim_comment').setup({
     comment_empty = false,
     create_mappings = false,
+    hook = function()
+        if check_mix_lang() then
+            require('ts_context_commentstring.internal').update_commentstring()
+        end
+    end,
 })
