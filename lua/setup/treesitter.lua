862193bde29e973a7a8b6e274b66a7f5d9e01175
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-08-11 18:47:11 +0700

    ✨ Add incremental selection

--- lua/setup/treesitter.lua
+++ lua/setup/treesitter.lua
@@ -5,4 +5,15 @@ treesitter_config.setup({
     highlight = { enable = true, use_languagetree = true },
     rainbow = { enable = true },
     autotag = { enable = true },
+    indent = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<M-i>',
+            node_incremental = '<Leader>k',
+            node_decremental = '<Leader>j',
+        },
+    },
 })
