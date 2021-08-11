3459667f967af02baeb5f4c29b31dc10d7148a24
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-08-11 22:03:56 +0700

    ⬇ Implement bufer delete

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,3 +1,9 @@
+local api = vim.api
+local cmd = vim.cmd
+local bo = vim.bo
+local lsp = vim.lsp
+local fn = vim.fn
+
 local M = {}
 
 -- Credit https://stackoverflow.com/questions/2705793/how-to-get-number-of-entries-in-a-lua-table
@@ -10,14 +16,14 @@ function M.tablelength(T)
 end
 
 -- Credit https://github.com/kosayoda/nvim-lightbulb/blob/master/lua/nvim-lightbulb.lua
-if vim.tbl_isempty(vim.fn.sign_getdefined('CodeActionSign')) then
-    vim.fn.sign_define('CodeActionSign', { text = '', texthl = 'LspDiagnosticsDefaultInformation' })
+if vim.tbl_isempty(fn.sign_getdefined('CodeActionSign')) then
+    fn.sign_define('CodeActionSign', { text = '', texthl = 'LspDiagnosticsDefaultInformation' })
 end
 
 -- Credit https://github.com/kosayoda/nvim-lightbulb/blob/master/lua/nvim-lightbulb.lua
 local function _update_sign(priority, old_line, new_line)
     if old_line then
-        vim.fn.sign_unplace('code_action', { id = old_line, buffer = '%' })
+        fn.sign_unplace('code_action', { id = old_line, buffer = '%' })
 
         -- Update current lightbulb line
         vim.b.lightbulb_line = nil
@@ -25,7 +31,7 @@ local function _update_sign(priority, old_line, new_line)
 
     -- Avoid redrawing lightbulb if code action line did not change
     if new_line and (vim.b.lightbulb_line ~= new_line) then
-        vim.fn.sign_place(new_line, 'code_action', 'CodeActionSign', '%', { lnum = new_line, priority = priority })
+        fn.sign_place(new_line, 'code_action', 'CodeActionSign', '%', { lnum = new_line, priority = priority })
         -- Update current lightbulb line
         vim.b.lightbulb_line = new_line
     end
@@ -48,10 +54,76 @@ end
 
 -- Credit https://github.com/kosayoda/nvim-lightbulb/blob/master/lua/nvim-lightbulb.lua
 function M.code_action()
-    local context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
-    local params = vim.lsp.util.make_range_params()
+    local context = { diagnostics = lsp.diagnostic.get_line_diagnostics() }
+    local params = lsp.util.make_range_params()
     params.context = context
-    vim.lsp.buf_request(0, 'textDocument/codeAction', params, handler_factory(params.range.start.line))
+    lsp.buf_request(0, 'textDocument/codeAction', params, handler_factory(params.range.start.line))
+end
+
+-- Credit https://github.com/famiu/bufdelete.nvim/blob/master/lua/bufdelete/init.lua
+function M.bufdelete(bufnr, force)
+    -- If buffer is modified and force isn't true, print error and abort
+    if not force and bo.modified then
+        return api.nvim_err_writeln(
+            string.format('No write since last change for buffer %d (set force to true to override)', bufnr)
+        )
+    end
+
+    if bufnr == 0 or bufnr == nil then
+        bufnr = api.nvim_get_current_buf()
+    end
+
+    local kill_command = 'bd'
+
+    if force then
+        kill_command = kill_command .. '!'
+    end
+
+    -- Get list of windows IDs with the buffer to close
+    local windows = vim.tbl_filter(function(win)
+        return api.nvim_win_get_buf(win) == bufnr
+    end, api.nvim_list_wins())
+
+    if #windows == 0 then
+        return
+    end
+
+    -- Get list of active buffers
+    local buffers = vim.tbl_filter(function(buf)
+        return bo[buf].buflisted and api.nvim_buf_is_valid(buf)
+    end, api.nvim_list_bufs())
+
+    -- If there is only one buffer (which has to be the current one), vim will
+    -- create a new buffer on :bd.  If there are only two buffers (one of which
+    -- has to be the current one), vim will switch to the other buffer on :bd.
+    -- Otherwise, pick the next buffer (wrapping around if necessary)
+    if #buffers > 2 then
+        for i, v in ipairs(buffers) do
+            if v == bufnr then
+                local next_buffer = buffers[i % #buffers + 1]
+                for _, win in ipairs(windows) do
+                    api.nvim_win_set_buf(win, next_buffer)
+                end
+            end
+        end
+    end
+
+    cmd(string.format('%s %d', kill_command, bufnr))
+end
+
+function M.dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. '[' .. k .. '] = ' .. M.dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
 end
 
 return M
