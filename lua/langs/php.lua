ee5dca68d4c3dadde605737ca31abc15f80772fd
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-08-17 16:14:54 +0700

    🔥 Remove langs directory

--- lua/langs/php.lua
+++
@@ -1,6 +0,0 @@
-local lsp = require('../lsp')
-
-require('lspconfig').phpactor.setup({
-    capabilities = lsp.capabilities,
-    on_attach = lsp.on_attach,
-})
--- lua/langs/python.lua
+++
@@ -1,6 +0,0 @@
-local lsp = require('../lsp')
-
-require('lspconfig').pyright.setup({
-    capabilities = lsp.capabilities,
-    on_attach = lsp.on_attach,
-})
