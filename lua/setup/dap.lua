a9ca73ee133317f397f70c92378aa265b1a53c48
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2022-02-13 19:35:22 +0700

    ✨ Support debug in node

--- lua/setup/dap.lua
+++ lua/setup/dap.lua
@@ -99,4 +99,30 @@ dap.configurations.go = {
     },
 }
 
+dap.adapters.node2 = {
+    type = 'executable',
+    command = 'node',
+    args = { os.getenv('HOME') .. '/.local/share/dap/vscode-node-debug2/out/src/nodeDebug.js' },
+}
+
+dap.configurations.javascript = {
+    {
+        name = 'Launch',
+        type = 'node2',
+        request = 'launch',
+        program = '${file}',
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = 'inspector',
+        console = 'integratedTerminal',
+    },
+    {
+        -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+        name = 'Attach to process',
+        type = 'node2',
+        request = 'attach',
+        processId = require('dap.utils').pick_process,
+    },
+}
+
 require('nvim-dap-virtual-text').setup()
